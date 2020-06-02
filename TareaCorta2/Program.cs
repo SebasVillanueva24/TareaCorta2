ce62005b8b8053c3dc2afb6a718043389faa4062
Sebastián Villanueva <elsebuchas@hotmail.com>
2020-06-02 03:15:41 -0600

    Primer commit

---
+++ TareaCorta2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReinasProbabilistico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TareaCorta2/Tablero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace ReinasProbabilistico
+{
+	public class Tablero
+	{
+		Stack posicionesProhibidas = new Stack();
+
+		public void nReinas(int N)
+		{
+			bool[, ] tablero = new bool[N,N];
+
+
+		}
+	}
+}
+
