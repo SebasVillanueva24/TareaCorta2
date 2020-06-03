b7c3dd8a4d83c7584949adf12b7ec9f3048eba65
Sebastián Villanueva <elsebuchas@hotmail.com>
2020-06-03 03:53:37 -0600

    Diagonales
    Guarda las diagonales

--- TareaCorta2/Tablero.cs
+++ TareaCorta2/Tablero.cs
@@ -40,24 +40,39 @@ namespace ReinasProbabilistico
 			for(int i = 0; i < N; i++)
 			{
 				Random rnd = new Random();
-				int columna = rnd.Next(0, 4);
+				int columna = rnd.Next(0, N);
 
 				if (i == 0)
 				{
-					;
+					
 					reinasColocadas.Push((i,columna));
 					int varx = i;
 					int vary = columna;
 
-					for (int k = 0; k < N; k++)
+					for (int k = 0; k <= N; k++)
 					{
-						if(varx+1 < N && vary+1 < N)
+						if (varx + 1 <= N && vary + 1 <= N)
 						{
+							Console.WriteLine(k);
+							Console.WriteLine(N);
 							posicionesProhibidas.Push((varx++, vary++));
 						}
-						
-					}
 
+					}
+					int varx2 = i;
+					int vary2 = columna;
+
+
+					for (int k = columna+1; k >= 0; k--)
+					{
+						if ( vary2 >= 0)
+						{
+							Console.WriteLine(varx2);
+							Console.WriteLine(vary2);
+							posicionesProhibidas.Push((varx2++, vary2--));
+						}
+
+					}
 					tablero[i, columna] = true;
 					//Console.WriteLine("Posicion del true" + i + " " + columna);
 				}
@@ -67,22 +82,22 @@ namespace ReinasProbabilistico
 					{
 						while(true)
 						{
-							columna = rnd.Next(0, 4);
+							columna = rnd.Next(0, N);
 							if(validarColumna(columna) == true)
 							{
 								break;
 							}
 						}
 
-						Console.WriteLine(validarColumna(columna));
-						Console.Write(Environment.NewLine + Environment.NewLine);
+						//Console.WriteLine(validarColumna(columna));
+						//Console.Write(Environment.NewLine + Environment.NewLine);
 						tablero[i, columna] = true;
 						reinasColocadas.Push((i, columna));
 					}
 					else
 					{
-						Console.WriteLine(validarColumna(columna));
-						Console.Write(Environment.NewLine + Environment.NewLine);
+						//Console.WriteLine(validarColumna(columna));
+						//Console.Write(Environment.NewLine + Environment.NewLine);
 						tablero[i, columna] = true;
 						reinasColocadas.Push((i, columna));
 					}
@@ -91,7 +106,7 @@ namespace ReinasProbabilistico
 			}
 			imprimirMatriz(tablero, N);
 
-			foreach(ValueTuple<int,int> elemento in posicionesProhibidas)
+			foreach (ValueTuple<int, int> elemento in posicionesProhibidas)
 			{
 				Console.WriteLine(elemento);
 			}
